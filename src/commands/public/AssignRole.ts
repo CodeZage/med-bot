bbdd9bc0b45ef6a880e4b25c4e96833020983b8f
Hector <hectorthoegersen@gmail.com>
2021-02-21 23:04:40 +0100

    Updated AssignRole command

--- src/commands/public/AssignRole.ts
+++ src/commands/public/AssignRole.ts
@@ -17,6 +17,7 @@ export default class AssignRole extends Command {
             args: [
                 {
                     id: "member",
+                    type: "member",
                     default: (msg: Message) => msg.member,
                 },
                 {
@@ -32,7 +33,7 @@ export default class AssignRole extends Command {
                         return null;
                     },
                     match: "option",
-                    flag: ["-year="],
+                    flag: "--year",
                     default: 0,
                 },
             ],
@@ -42,50 +43,65 @@ export default class AssignRole extends Command {
     public exec(
         message: Message,
         { member, year }: { member: GuildMember; year: Number }
-    ) {
+    ): Promise<Message> {
+        message.delete({ timeout: 500, reason: "Automatic Removal" });
+
+        let timout = 8000;
+
         switch (year) {
             case 1:
                 member.roles.add("813118674642403343").catch(console.error);
-                message.util.send(
-                    `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 1st year student 😄`
-                );
-                break;
+
+                return message.util
+                    .send(
+                        `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 1st year student 😄`
+                    )
+                    .then((message) => message.delete({ timeout: timout }));
+
             case 2:
                 member.roles.add("813124643799302146").catch(console.error);
-                message.util.send(
-                    `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 2nd year student 😄`
-                );
-                break;
+                return message.util
+                    .send(
+                        `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 2nd year student 😄`
+                    )
+                    .then((message) => message.delete({ timeout: timout }));
+
             case 3:
                 member.roles.add("813124991641845771").catch(console.error);
-                message.util.send(
-                    `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 3rd year student 😄`
-                );
-                break;
+                return message.util
+                    .send(
+                        `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 3rd year student 😄`
+                    )
+                    .then((message) => message.delete({ timeout: timout }));
+
             case 4:
                 member.roles.add("813127288836849694").catch(console.error);
-                message.util.send(
-                    `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 4th year student 😄`
-                );
-                break;
+                return message.util
+                    .send(
+                        `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 4th year student 😄`
+                    )
+                    .then((message) => message.delete({ timeout: timout }));
+
             case 5:
                 member.roles.add("813131657791012884").catch(console.error);
-                message.util.send(
-                    `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 5th year student 😄`
-                );
-                break;
+                return message.util
+                    .send(
+                        `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 5th year student 😄`
+                    )
+                    .then((message) => message.delete({ timeout: timout }));
+
             case 6:
                 member.roles.add("813132811661869066").catch(console.error);
-                message.util.send(
-                    `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 6th year student 😄`
-                );
-                break;
+                return message.util
+                    .send(
+                        `SUCCESS - Welcome \`${member.user.tag}\` you are now considered a 6th year student 😄`
+                    )
+                    .then((message) => message.delete({ timeout: timout }));
 
             default:
-                message.util.send(
-                    `ERROR - Sorry \`${member.user.tag}\` the year you have selected is invalid`
+                return message.util.send(
+                    `ERROR - Sorry \`${member.user.tag}\` what you have entered doesn't appear to be a valid command`
                 );
-                break;
         }
     }
 }
